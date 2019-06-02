2fb43fc1f7dc9da7a3d3b9ff7e80fb7b78766340
Becky Phan <beckyphan@live.com>
2019-06-02 23:26:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,13 +58,13 @@ class Song
     def self.artist_count 
     artists = {}
     @@artists.each do |artist|
-      if artist.include?(genre)
-        genres[genre] += 1
+      if artists.include?(artist)
+        artists[artist] += 1
       else 
-        genres[genre] = 1
+        artists[artist] = 1
       end
     end
-    genres
+    artists
   end
   
 end
